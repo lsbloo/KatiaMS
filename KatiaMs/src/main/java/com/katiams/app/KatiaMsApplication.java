8c8b9a225556d8dcd7cebf33f5056d550647d672
unknown <osvaldo.cavalcanti_filho@hotmail.com>
2018-11-15 22:50:01 -0200

    piloto

---
+++ KatiaMs/src/main/java/com/katiams/app/KatiaMsApplication.java
@@ -0,0 +1,16 @@
+package com.katiams.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatiaMsApplication {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// Springboot Application;
+		SpringApplication.run(KatiaMsApplication.class, args);
+	}
+}
---
+++ KatiaMs/src/test/java/com/katiams/app/KatiaMsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.katiams.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KatiaMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
